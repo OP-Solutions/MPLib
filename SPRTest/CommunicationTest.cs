d75d5da77010e9b543467ab583c9d9e9c6525a43
giorgi chxikvadze <giorgi_chxikva@outlook.com>
2019-08-31 21:51:21 +0400

    Minor changes

--- SPRTest/CommunicationTest.cs
+++ SPRTest/CommunicationTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SPR.Models;
@@ -16,6 +17,9 @@ namespace SPRTest
         [DataRow("94.100.237.197", 87, "bakurits", "Chudo123")] // from kukur
         public void TestP2P(string targetIp, int targetPort, string messageToSend, string messageToReceive)
         {
+            var time = DateTime.UtcNow;
+            var time2 = new DateTime(time.Year, time.Month, time.Day, time.Hour, time.Minute + 1, 0);
+            Thread.Sleep(time2 - time);
             var remoteClient = new RemoteClient(null, null, new IPEndPoint(IPAddress.Parse(targetIp), targetPort));
             remoteClient.Connect();
             var stream = remoteClient.GetStream();
