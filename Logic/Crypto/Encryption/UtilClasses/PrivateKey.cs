f29eb0184950422baea05185da7161d0e170963c
dbejanishvili <datobejanishvili@gmail.com>
2020-06-26 17:18:03 +0400

    Encryption initial commit

---
+++ Logic/Crypto/Encryption/UtilClasses/PrivateKey.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+
+namespace SPR.Crypto.Encryption.UtilClasses
+{
+    public struct PrivateKey
+    {
+        public BigInteger D { get; }
+        public BigInteger N { get; }
+
+        public PrivateKey(BigInteger d, BigInteger n)
+        {
+            this.D = d;
+            this.N = n;
+        }
+
+    }
+}
---
+++ Logic/Crypto/Encryption/UtilClasses/PublicKey.cs
@@ -0,0 +1,16 @@
+﻿using System.Numerics;
+
+namespace SPR.Crypto.Encryption.UtilClasses
+{
+    public class PublicKey
+    {
+        public BigInteger E { get; }
+        public BigInteger N { get; }
+
+        public PublicKey(BigInteger e, BigInteger n)
+        {
+            this.E = e;
+            this.N = n;
+        }
+    }
+}
